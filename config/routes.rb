a96e09ff75814cc88a3d9e7ed77f7ea7ee4d9937
Junior Aguilar <juniorjose.aguilarparra@gmail.com>
2023-01-10 17:57:53 +0100

    create route to api

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+
+  namespace :api do
+    namespace :v1, defaults: { format: 'json' } do
+      get '/search', to: 'products#search'
+    end
+  end
 end
